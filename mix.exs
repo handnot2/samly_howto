a17adab12d47aec1b6b4b6bc5c4c8d46d90567de
handnot2 <handnot2@gmail.com>
2017-09-14 17:01:06 -0700

    updated version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SamlyHowto.Mixfile do
   def project do
     [
       app: :samly_howto,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
